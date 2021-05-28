7b6c60f5281d17803f1f0a17323d3cdabe631dfe
José Luiz Borbolato <luizborbolato@gmail.com>
2021-05-28 00:59:51 -0300

    Commit do Olá mundo DJANGO

--- app.py
+++ app.py
@@ -1,1 +1,5 @@
-print('Olá mundo')
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+print(BASE_DIR)
---
+++ repasseFies/fies/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FiesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'fies'
